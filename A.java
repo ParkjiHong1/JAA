e21dc397460136a2dd8eefa726e4ae938514e582
PrkJi <geone11@naver.com>
2017-10-29 07:01:03 +0900

    C

--- A.java
+++ A.java
@@ -1,5 +1,7 @@
 public class A {
    public static void main(String[] args) {
-      System.out.println("Hello, world from GIT!");
+      System.out.println("Hello, world from A!");
+      System.out.println("Hello, world from B!");
+      System.out.println("Hello, world from C!");
    }
 }
